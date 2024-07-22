a8499cf2e7c97e913cde4cc5cbaff2fe1c7cdf1b
Kutlu San Demiroren <Demiroren@student.tudelft.nl>
2024-07-22 14:18:08 +0200

    add basics

---
+++ src/main/java/GameProperties/Tile.java
@@ -0,0 +1,7 @@
+package main.java.GameProperties;
+
+public class Tile{
+    private TileColor tileColor;
+    private byte number;
+    
+}
---
+++ src/main/java/GameProperties/TileColor.java
@@ -0,0 +1,5 @@
+package main.java.GameProperties;
+
+public enum TileColor {
+    RED,BLUE,ORANGE,GREEN,JOKER
+}
